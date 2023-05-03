9d593141951780dea206a71daf4876a651ed6dc1
Viktoriia Ustymova <svi.3481@gmail.com>
2023-05-03 20:40:44 +0200

    add task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        //Напишите метод indexOf, который возвращает индекс заданного символа в заданной строке.
+        // Т.е. метод принимает строку и char и возвращает позиуию,
+        // где этот символ первый раз встречается в строке.
+        //Если в заданной строке нет заданного символа, то метод должен вернуть -1.
+        //Примеры: indexOf("Java is the best", 'i') -> 5
+        //indexOf("Java is the best", 'q') -> -1
+        //indexOf("Java is the best", 'a') -> 1
+
     }
 }
